b95bdb8cbbfd2dc7aabfd4485cd70afaff058e84
Jordan Farthing <jfarthing@extron.com>
2025-03-21 10:19:40 -0400

    Angular @for @empty, $index and other extra options

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -13,6 +13,9 @@ export class CourseCardComponent {
   })
   course:Course;
 
+  @Input({required:true})
+  index:number;
+
   @Output('courseSelected')
   courseEmitter = new EventEmitter<Course>();
 

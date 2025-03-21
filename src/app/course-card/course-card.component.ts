ba81aefd60da04f46cd378f9e14e6c650451d1b3
Jordan Farthing <jfarthing@extron.com>
2025-03-21 11:22:50 -0400

    The Angular ngFor Core Directive

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -14,7 +14,7 @@ export class CourseCardComponent {
   course:Course;
 
   @Input({required:true})
-  index:number;
+  cardIndex:number;
 
   @Output('courseSelected')
   courseEmitter = new EventEmitter<Course>();

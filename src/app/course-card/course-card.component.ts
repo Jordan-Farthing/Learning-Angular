ce6751c874af38e7648de526de1ace15d48fdae4
Jordan Farthing <jfarthing@extron.com>
2025-03-25 12:25:17 -0400

    Angular ngIf Directive and the Elvis Operator

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'course-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
@@ -23,6 +24,10 @@ export class CourseCardComponent {
     console.log("card component - button clicked ...");
 
     this.courseEmitter.emit(this.course);
+  }
+
+  isImageVisibile(){
+    return this.course && this.course.iconUrl
   }
 
 }

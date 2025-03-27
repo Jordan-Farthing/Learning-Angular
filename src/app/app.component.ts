40c968b3d92f776a94ef4434cd0850df7efc89f4
Jordan Farthing <jfarthing@extron.com>
2025-03-27 12:44:16 -0400

    Angular Built-In Pipes - Complete Catalog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,12 @@ export class AppComponent {
 
     courses = COURSES;
 
+    title = COURSES[0].description;
+    price = 9.99243213252;
+    startDate = new Date(2000,0,1);
+    rate = 0.67;
+    course = COURSES[0];
+
     onCourseSelected(course:Course){
         console.log("App component - click event bubbled...", course);
     }

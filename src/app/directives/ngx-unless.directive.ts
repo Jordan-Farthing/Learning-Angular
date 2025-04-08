647c0512498289e46d7b8f6528f1d68e1bffc0b8
Jordan Farthing <jordie@intrstar.net>
2025-04-08 10:08:04 -0400

    Angular Structural Directives - Step-by-Step Implementation

--- src/app/directives/ngx-unless.directive.ts
+++ src/app/directives/ngx-unless.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[ngxUnless]'
 })
 export class NgxUnlessDirective {
 
-  constructor() { }
+  visible = false;
+
+  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {
+  }
+
+  @Input()
+  set ngxUnless(condition:boolean) {
+    if(!condition && !this.visible){
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.visible = true;
+    }
+    else if(condition && this.visible){
+      this.viewContainer.clear();
+      this.visible = false;
+    }
+
+  }
 
 }
